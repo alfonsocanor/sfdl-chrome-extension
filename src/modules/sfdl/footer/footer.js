fa41fe244148ddeec02f9798d8a06f260dc5f391
zkno <zkno@MacBook-Pro.local>
2023-04-14 16:43:57 +0200

    general updates

--- src/modules/sfdl/footer/footer.js
+++ src/modules/sfdl/footer/footer.js
@@ -1,5 +1,14 @@
 import { LightningElement } from 'lwc';
 
+const sfdlPage = [
+    {
+        title: 'sfdl chrome extension github repository',
+        iconTitle:'sfdl logo',
+        src: './resources/sfdl.png',
+        alt: 'sfdl logo',
+        url: 'https://salesforcedebuglogs.com'
+    }
+];
 export default class Footer extends LightningElement{
-   
+    sfdlPage = sfdlPage;
 }
