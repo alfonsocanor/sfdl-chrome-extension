f29e671664f669ccbd6ea8148d8f0efba4fc365b
zkno <zkno@zk-asdf.local>
2024-10-26 18:55:11 +0200

    logo-display

--- src/modules/sfdl/launchStation/launchStation.js
+++ src/modules/sfdl/launchStation/launchStation.js
@@ -1,5 +1,6 @@
 /*global chrome*/
 import { LightningElement } from 'lwc';
+import { css } from './launchStationCss';
 
 export default class App extends LightningElement {
     isConsole;
@@ -8,6 +9,32 @@ export default class App extends LightningElement {
     connectedCallback(){    
         this.isPopup = window.location.pathname === '/popup.html';
         this.isConsole = !this.isPopup;
+
+        if(this.isConsole) {
+            this.showSfdlLogo();
+        }
+    }
+
+    showSfdlLogo() {
+        this.initializeCustomCss();
+        setTimeout(() => {
+            let logo = this.template.querySelector('.sfdl-logo');
+            if(!logo.classList.contains('fade-out')) {            
+                logo.classList.add('fade-out');
+                setTimeout(() => {
+                    logo.remove();
+                }, 1000);
+            }
+        }, 50);
+    }
+
+    initializeCustomCss() { 
+        // Check if the custom CSS is already loaded
+        if (!document.adoptedStyleSheets.some(sheet => sheet instanceof CSSStyleSheet && sheet.replaceSync === css.replaceSync)) {
+            const sheet = new CSSStyleSheet();
+            sheet.replaceSync(css);
+            document.adoptedStyleSheets = [...document.adoptedStyleSheets, sheet];
+        }
     }
 
     openSfdlAppInANewTab(){
---
+++ src/modules/sfdl/launchStation/launchStationCss.js
@@ -0,0 +1,14 @@
+export const css = `
+    .fade-out {
+        animation: fadeOut 1s forwards;
+    }
+
+    @keyframes fadeOut {
+        0% {
+            opacity: 1;
+        }
+        100% {
+            opacity: 0;
+        }
+    }
+`;
